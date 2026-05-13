0bba09cd6e86590b127aae5a660cbca68ed0c4dd
Graham Keenan <graham.keenan@outlook.com>
2026-05-13 23:55:49 +0100

    docs: more object type docs

--- src/objects/bulk.rs
+++ src/objects/bulk.rs
@@ -1,21 +1,46 @@
+//! Documentation relating to the [`BulkData`] object in Scryfall
+//!
+//! More detailed information can be found here: <https://scryfall.com/docs/api/bulk-data>
 use super::types::BulkDataType;
 use jiff::Timestamp;
 use serde::{Deserialize, Serialize};
 use url::Url;
 use uuid::Uuid;
 
+/// Data relating to the Bulk Data requested from Scryfall
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct BulkData {
+    /// Content type of the object, always `bulk_data`
     pub object: String,
+
+    /// Unique ID for this bulk item
     pub id: Uuid,
+
+    /// Scryfall API URI for this file
     pub uri: Url,
+
+    /// Type of bulk data requested
     #[serde(rename = "type")]
     pub bulk_type: BulkDataType,
+
+    /// Name of the file
     pub name: String,
+
+    /// Description for the file
     pub description: String,
+
+    /// URI that hosts the file for fetching
     pub download_uri: Url,
+
+    /// Timestamp for when the file was last updated
     pub updated_at: Timestamp,
+
+    /// Size of the file in bytes
     pub size: usize,
+
+    /// MIME-type of the file
     pub content_type: String,
+
+    /// Encoding that is used to transmit the file when it is downloaded
     pub content_encoding: String,
 }
--- src/objects/catalog.rs
+++ src/objects/catalog.rs
@@ -1,10 +1,21 @@
+//! Documentation relating to the [`Catalog`] object in Scryfall
+//!
+//! More detailed information can be found here: <https://scryfall.com/docs/api/catalogs>
 use serde::{Deserialize, Serialize};
 use url::Url;
 
+/// A collection of Magic datapoints (words, card values, etc)
 #[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Catalog {
+    /// Content type for the object, always `catalog`
     pub object: String,
+
+    /// Link to the current catalog on Scryfall's API
     pub uri: Url,
+
+    /// Number of items in the `data` array
     pub total_values: u32,
+
+    /// Array of data points
     pub data: Vec<String>,
 }
