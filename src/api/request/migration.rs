8a58b0da99a326874095391e8ae4c870ae73b2cd
Graham Keenan <graham.keenan@outlook.com>
2026-05-19 17:55:54 +0100

    docs: added docs for the migration requests

--- src/api/request/migration.rs
+++ src/api/request/migration.rs
@@ -1,3 +1,6 @@
+//! Request types and builders for Migrations in Scryfall API
+//!
+//! More information can be found here: <https://scryfall.com/docs/api/migrations>
 use std::str::FromStr;
 
 use super::{BASE_URL, ScryfallApiError, ScryfallRequest};
@@ -6,12 +9,14 @@ use crate::objects::{CardMigration, List};
 use url::Url;
 use uuid::Uuid;
 
+/// Request type for calling the `/migrations` Scryfall endpoint
 #[derive(Debug, Clone)]
 pub struct MigrationRequest {
     page: u32,
 }
 
 impl MigrationRequest {
+    /// Constructs a builder for a `MigrationRequest`
     pub fn builder() -> MigrationRequestBuilder {
         MigrationRequestBuilder::default()
     }
@@ -31,32 +36,38 @@ impl ScryfallRequest for MigrationRequest {
     }
 }
 
+/// Builder for constructing a [`MigrationRequest`]
 #[derive(Default)]
 pub struct MigrationRequestBuilder {
     page: u32,
 }
 
 impl MigrationRequestBuilder {
+    /// Constructs a new `MigrationRequestBuilder`
     pub fn new(page: u32) -> Self {
         Self { page }
     }
 
+    /// Sets the `page` number to request for the Migration
     pub fn page(mut self, page: u32) -> Self {
         self.page = page;
         self
     }
 
+    /// Builds the [`MigrationRequest`]
     pub fn build(self) -> Result<MigrationRequest, ScryfallApiError> {
         Ok(MigrationRequest { page: self.page })
     }
 }
 
+/// Request type for calling the `/migrations/:id` Scryfall endpoint
 #[derive(Debug, Clone)]
 pub struct MigrationByIdRequest {
     id: Uuid,
 }
 
 impl MigrationByIdRequest {
+    /// Constructs a builder for a `MigrationByIdRequest`
     pub fn builder() -> MigrationByIdRequestBuilder {
         MigrationByIdRequestBuilder::default()
     }
@@ -74,20 +85,24 @@ impl ScryfallRequest for MigrationByIdRequest {
     }
 }
 
+/// Builder for constructing a [`MigrationByIdRequest`]
 pub struct MigrationByIdRequestBuilder {
     id: Uuid,
 }
 
 impl MigrationByIdRequestBuilder {
+    /// Constructs a new `MigrationByIdRequestBuilder`
     pub fn new(id: Uuid) -> Self {
         Self { id }
     }
 
+    /// Sets the ID for the Migration to request
     pub fn id(mut self, id: Uuid) -> Self {
         self.id = id;
         self
     }
 
+    /// Builds the [`MigrationByIdRequest`]
     pub fn build(self) -> Result<MigrationByIdRequest, ScryfallApiError> {
         Ok(MigrationByIdRequest { id: self.id })
     }
