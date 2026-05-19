118f8c94a8ecdb4e533bb47dfeb4322573522991
Graham Keenan <graham.keenan@outlook.com>
2026-05-19 17:47:35 +0100

    docs: added docs for catalog requests

--- src/api/request/catalog.rs
+++ src/api/request/catalog.rs
@@ -1,14 +1,19 @@
+//! Request types and builders for Catalogs in Scryfall API
+//!
+//! More details can be found here: <https://scryfall.com/docs/api/catalogs>
 use crate::objects::catalog::Catalog;
 
 use super::{BASE_URL, ScryfallApiError, ScryfallRequest};
 use url::Url;
 
+/// Request type for calling the `/catalog/:catalog_type` Scryfall endpoint
 #[derive(Debug, Clone)]
 pub struct CatalogRequest {
     catalog_type: CatalogType,
 }
 
 impl CatalogRequest {
+    /// Constructs a new builder for a `CatalogRequest`
     pub fn builder() -> CatalogRequestBuilder {
         CatalogRequestBuilder::default()
     }
@@ -26,21 +31,25 @@ impl ScryfallRequest for CatalogRequest {
     }
 }
 
+/// Builder for constructing a [`CatalogRequest`]
 #[derive(Default)]
 pub struct CatalogRequestBuilder {
     catalog_type: CatalogType,
 }
 
 impl CatalogRequestBuilder {
+    /// Constructs a new `CatalogRequestBuilder`
     pub fn new(catalog_type: CatalogType) -> Self {
         Self { catalog_type }
     }
 
+    /// Sets the [`CatalogType`] for the request
     pub fn catalog_type(mut self, catalog_type: CatalogType) -> Self {
         self.catalog_type = catalog_type;
         self
     }
 
+    /// Builds the [`CatalogRequest`]
     pub fn build(self) -> Result<CatalogRequest, ScryfallApiError> {
         Ok(CatalogRequest {
             catalog_type: self.catalog_type,
@@ -48,28 +57,49 @@ impl CatalogRequestBuilder {
     }
 }
 
+/// Catalog types that can be used in [`CatalogRequest`]
 #[derive(Debug, Copy, Clone, PartialEq, Default)]
 pub enum CatalogType {
+    /// Use the `/catalog/card-names` endpoint
     #[default]
     CardNames,
+    /// Use the `/catalog/artist-names` endpoint
     ArtistNames,
+    /// Use the `/catalog/word-bank` endpoint
     WordBank,
+    /// Use the `/catalog/supertypes` endpoint
     SuperTypes,
+    /// Use the `/catalog/card-types` endpoint
     CardTypes,
+    /// Use the `/catalog/artifact-types` endpoint
     ArtifactTypes,
+    /// Use the `/catalog/battle-types` endpoint
     BattleTypes,
+    /// Use the `/catalog/creature-types` endpoint
     CreatureTypes,
+    /// Use the `/catalog/enchantment-types` endpoint
     EnchantmentTypes,
+    /// Use the `/catalog/land-types` endpoint
     LandTypes,
+    /// Use the `/catalog/planeswalker-types` endpoint
     PlaneswalkerTypes,
+    /// Use the `/catalog/spell-types` endpoint
     SpellTypes,
+    /// Use the `/catalog/powers` endpoint
     Powers,
+    /// Use the `/catalog/toughnesses` endpoint
     Toughnesses,
+    /// Use the `/catalog/loyalties` endpoint
     Loyalties,
+    /// Use the `/catalog/keyword-abilities` endpoint
     KeywordAbilities,
+    /// Use the `/catalog/keyword-actions` endpoint
     KeywordActions,
+    /// Use the `/catalog/ability-words` endpoint
     AbilityWords,
+    /// Use the `/catalog/flavor-words` endpoint
     FlavorWords,
+    /// Use the `/catalog/watermarks` endpoint
     Watermarks,
 }
 
